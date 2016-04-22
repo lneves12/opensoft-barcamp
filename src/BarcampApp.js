055309e09b9462052ea7f2a655d179344c28a0a8
Luis Neves <luis.neves12@gmail.com>
2016-04-22 02:06:07 +0100

    First draft for getting data from vimeo api and show a video preview before playing it

--- src/BarcampApp.js
+++ src/BarcampApp.js
@@ -6,72 +6,117 @@ const  {
     DrawerLayoutAndroid,
     StatusBar,
     WebView,
+    Image,
     StyleSheet,
-    Dimensions
+    TouchableOpacity,
+    ProgressBarAndroid
 } = require('react-native');
-
-var {height, width} = Dimensions.get('window')
-
 
 const leftMenuApi = require('./LeftMenuAndroid/leftMenuApi');
 
 const LeftMenu = require('./LeftMenuAndroid/LeftMenu');
 const BarcampHeader = require('./common/BarcampHeader');
 
-var BarcampApp = React.createClass({
 
-    getInitialState(){
-        return this.state = {
-            w:width,
-            h:height
-        }
+var BarcampApp = React.createClass({
+  
+    getInitialState() {
+      return {}
+    },
+     
+    componentWillMount() {
+        let videoId = 128780049;
+        
+        fetch(`http://www.vimeo.com/api/v2/video/${videoId}.json`, {
+            method: 'get'
+        }).then((response) => {
+            return response.json();
+        }).then((jsonResponse) => {
+            this.setState({
+                videoPreviewImageUrl: jsonResponse[0].thumbnail_large
+            }) 
+        }).catch((err) => {
+            console.log('why!! -' + err)
+        });
+    },  
+ 
+    hasEnoughData() {
+      return this.state.videoPreviewImageUrl;  
+    },   
+    
+    onVideoPress() {
+        this.setState({isVideoOn: true})
     },
 
     render() {
         
-        const navigationView = (
-            <LeftMenu style={styles.container}> </LeftMenu>
-        );
-        
+                             
         const handleDrawerInit = (drawer) => {
             leftMenuApi.init(drawer)
         }; 
         
-        // source={{uri: 'https://player.vimeo.com/video/128780049?title=0&byline=0&badge=0&portrait=0'}}
-        // source={{html: '<iframe src="https://player.vimeo.com/video/128780049?title=0&byline=0&badge=0&portrait=0" width="{asd}" height="{asd}" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'}}
-        var videoHeight= this.state.h - 81; 
-        
-        return(   
-            <View style={styles.container} onLayout={this.handleChangeLayout}> 
-                <StatusBar
-                    translucent={true}
-                    backgroundColor="rgba(0, 0, 0, 0.2)"
-                />
-                <DrawerLayoutAndroid   
-                    ref={(drawer) => handleDrawerInit(drawer)}
-                    drawerWidth={300}
-                    drawerPosition={DrawerLayoutAndroid.positions.Left}
-                    renderNavigationView={() => navigationView}>
-                        <BarcampHeader 
-                            title='2016 - South Side of Life'
-                        />  
-                        <WebView
-                            style={{flex: 1}}
-                            javaScriptEnabled={true}
-                            source={{html: '<style> html, body {border: 0px;margin: 0px;padding: 0px;} </style> <iframe src="https://player.vimeo.com/video/128780049?title=0&byline=0&badge=0&portrait=0" width="' + this.state.w +'" height=' + videoHeight + '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'}}  
-                        />
-                </DrawerLayoutAndroid>
-            </View>
-        )
+        const navigationView = (
+            <LeftMenu style={styles.container}> </LeftMenu>
+        );
+         
+        let renderImage = () => {
+            if(this.hasEnoughData()){
+                return( 
+                    <TouchableOpacity 
+                        style={styles.container}
+                        onPress={this.onVideoPress}>
+                            <Image
+                                style={styles.previewImage}
+                                source={{uri: this.state.videoPreviewImageUrl}}>
+                                    <Image
+                                        style={styles.playIcon}
+                                        source={require('./playIcon.png')}/>
+                            </Image> 
+                    </TouchableOpacity> 
+                )
+            } else {
+                return (
+                    <ProgressBarAndroid />
+                ) 
+            }     
+        }
+          
+        if(!this.state.isVideoOn){
+            return(   
+                <View style={styles.container}> 
+                    <StatusBar
+                        translucent={true}
+                        backgroundColor="rgba(0, 0, 0, 0.2)"
+                    />
+                    <DrawerLayoutAndroid   
+                        ref={(drawer) => handleDrawerInit(drawer)}
+                        drawerWidth={300}
+                        drawerPosition={DrawerLayoutAndroid.positions.Left}
+                        renderNavigationView={() => navigationView}>
+                            <BarcampHeader 
+                                title='2016 - South Side of Life'
+                            />
+                            <View style={{flex: 1}}>
+                                {renderImage()} 
+                            </View> 
+                            <View style={{flex: 1}}>
+                            </View>  
+                    </DrawerLayoutAndroid>
+                </View> 
+            )
+        } else {
+            return(
+                 <WebView
+                    style={{flex: 1}}
+                    javaScriptEnabled={true}
+                    source={{uri: 'https://player.vimeo.com/video/128780049?title=0&byline=0&badge=0&portrait=0&autoplay=1'}}
+                 />
+            )
+        }
     },
+
     
-    handleChangeLayout(event){
-        var {x, y, width, height} = event.nativeEvent.layout;
-        this.setState({
-            w:width,
-            h:height
-        })
-    }
+    
  
     
 });
@@ -79,7 +124,17 @@ var BarcampApp = React.createClass({
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  previewImage: {
+      flex:1,
+      alignItems: 'center',
+      justifyContent: 'center'
+  },
+  playIcon: {
+      height: 100,
+      width: 100,
+      opacity: 0.5
   }
-});
+}); 
 
 module.exports = BarcampApp;
